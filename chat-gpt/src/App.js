515b0cd26d975ccabf63b4aa107a2f9326abe0bc
Cynthia-Isutsa <isutsacynthia@gmail.com>
2023-07-19 13:16:04 +0300

    React file structure

---
+++ chat-gpt/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import Cta from './components/cta/Cta'
+import Feature from './components/feature/Feature'
+import Navbar from './components/navbar/Navbar'
+
+
+
+
+function App() {
+  return (
+    <div>
+        <h1>Chat gpt</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ chat-gpt/src/containers/whatGPT3/WhatGpt3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './WhatGpt3.css'
+
+const WhatGpt3 = () => {
+  return (
+    <div>
+      Whatgpt
+    </div>
+  )
+}
+
+export default WhatGpt3
